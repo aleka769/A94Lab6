820469e01bc1926c4bf8ad1bfbe86af926181267
Alexander Karlsson <aleka769@student.liu.se>
2018-10-11 16:52:03 +0200

    knapsack dynamic (without info about items)

---
+++ knapsack/R/knapsack_dynamic.R
@@ -0,0 +1,49 @@
+set.seed(42)
+n <- 2000
+knapsack_objects <-
+  data.frame(
+    w=sample(1:4000, size = n, replace = TRUE),
+    v=runif(n = n, 0, 10000)
+  )
+
+dynamic_knapsack <- function(x, W){
+  # Tests for input...
+  
+  value_mat <- matrix(NA, nrow = nrow(x) + 1, ncol = W + 1)
+  value_mat[1,] <- 0
+  rownames(value_mat) <- paste("i =", 0:(nrow(x)))
+  colnames(value_mat) <- paste("j =", 0:(W))
+  
+  x <- x[order(x$w),]
+  w <- x$w
+  v <- x$v
+  n <- nrow(x)
+  
+  for (i in 2:(n + 1)){
+    for (j in 2:(W + 2)){
+      if (w[i - 1] > j - 2){
+        value_mat[i, j - 1] = value_mat[i - 1, j - 1]
+      } else {
+        value_mat[i, j - 1] = max( ( value_mat[i - 1, j - 1] ),
+                                   ( value_mat[i - 1, j - 1 - w[i-1]] + v[i-1] ) )
+        
+      }
+    }
+  }
+  return(value_mat)
+}
+
+y <- data.frame(w = c(4,1,3,2,5,2),
+                v = c(7,3,6,9,2,4))
+dynamic_knapsack(x = y, W = 6)
+
+
+# index <- seq(3000, 10000, by = 20)
+# 
+# a <- lapply(index, FUN = function(i){
+#   microbenchmark::microbenchmark(dynamic_knapsack(x = knapsack_objects[1:8,], W = i), unit = "s", times = 1L)$time / 100000000
+# })
+# 
+# d <- data.frame(time = do.call(rbind, a), i = index)
+# 
+# ggplot(d, aes(i, time)) + geom_point()
