15adb63487d5136df521131f4c2b24f6d134eb9d
Napoleongl <guslu389@student.liu.se>
2018-10-11 22:00:15 +0200

    removed global var in funciton

--- knapsack/R/knapsack_brute_force.R
+++ knapsack/R/knapsack_brute_force.R
@@ -10,24 +10,29 @@
 #'
 #' @examples
 knapsack_brute_force <- function(x, W){
+  # error checks
   stopifnot(is.data.frame(x))
   stopifnot(c("v", "w") %in% names(x))
   stopifnot(is.numeric(x$w)&&is.numeric(x$v))
   stopifnot(any(x$w) <= W || any(x$v) > 0)
   
+  x <- as.matrix(x)
   n <- dim(x)[1]
   
+  # generating all combinations of elements as binary matrix
   combn_mat <- matrix(NA, nrow = 2^n, ncol = n)
   for (rownum in  0:(nrow(combn_mat)-1)){
     combn_mat[rownum+1, ] <- as.integer(intToBits(rownum))[1:n]
   }
   
-  stuff_mat <- as.matrix(stuff)
+  # result is matrix product of combinations and data
+  result_mat <- combn_mat %*% x
   
-  result_mat <- combn_mat %*% stuff_mat
+  # removing combinations with weight above maxweight
   result_mat[result_mat[,"w"] > W,2] <- NA
+  
   max_element <- which.max(result_mat[,"v"])
   
   list(element = seq(1,n,1)[combn_mat[max_element,] == 1L],
-       value = result_mat[max_element,"v"])
+       value = unname(result_mat[max_element,"v"]))
 }
