1aaeb0481d7504136c0e54728c04edc478504604
Napoleongl <guslu389@student.liu.se>
2018-10-12 23:15:09 +0200

    pre parallelization checkpoint

--- knapsack/R/knapsack_brute_force.R
+++ knapsack/R/knapsack_brute_force.R
@@ -9,7 +9,7 @@
 #' @export
 #'
 #' @examples
-brute_force_knapsack <- function(x, W, fast = FALSE, parallell = FALSE){
+brute_force_knapsack <- function(x, W, fast = FALSE, parallel = FALSE){
   if(fast){message("not yet implemented")}
   # error checks
   stopifnot(is.data.frame(x))
@@ -18,28 +18,34 @@ brute_force_knapsack <- function(x, W, fast = FALSE, parallell = FALSE){
   stopifnot(any(x$w <= W) || any(x$v > 0))
   stopifnot(W > 0)
   
-  x <- as.matrix(x)
+  x <- as.matrix(x[,c("v","w")])
   n <- dim(x)[1]
+  combo_count <- (2^n)-1
   
+  # checking if parallelization is requested and can be used
+  if (parallel & .Platform$OS.type == "unix"){ 
+    core_count <- parallel::detectCores()-1 
+  }
+  else if (parallel & .Platform$OS.type != "unix") {
+    core_count <- 1L 
+    message("Can't parallelize on non-unix OS, using one core!")
+  }
+  else {
+    core_count <- 1L
+  }
+
   # generating all combinations of elements as binary matrix
-  combn_mat <- matrix(NA, nrow = 2^n, ncol = n)
-  for (rownum in  0:(nrow(combn_mat)-1)){
+  combn_mat <- matrix(NA, nrow = combo_count, ncol = n)
+  for (rownum in  1:combo_count){
     combn_mat[rownum+1, ] <- as.integer(intToBits(rownum))[1:n]
   }
-  
+
   # result is matrix product of combinations and data
-  if(parallell){
-    result_mat <- parallel::mclapply(X=1:2^n, mc.cores = 1, FUN=function(row){
-      `%*%`(combn_mat[row,],x)})
-  } else {
-    result_mat <- combn_mat %*% x
-  }
-  
+  result_mat <- combn_mat %*% x
+
   # removing combinations with weight above maxweight
-  result_mat[result_mat[,"w"] > W,2] <- NA
-  
+  result_mat[result_mat[,"w"] > W, ] <- NA
   max_element <- which.max(result_mat[,"v"])
-  
-  list(value = unname(result_mat[max_element,"v"]),
-       elements = seq(1,n,1)[combn_mat[max_element,] == 1L])
+  return(list(value = unname(result_mat[max_element,"v"]),
+              elements = seq(1,n,1)[combn_mat[max_element,] == 1L]))
 }
