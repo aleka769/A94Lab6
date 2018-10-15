3598278ad5ada1cb4b9dfbcc10c7cf649c921d75
Napoleongl <guslu389@student.liu.se>
2018-10-15 12:57:42 +0200

    build

--- knapsack_brute_force_test.R
+++ knapsack_brute_force_test.R
@@ -1,9 +1,11 @@
 W <- 25
 
 set.seed(831117)
-n <- 20
+n <- 1000000
 stuff <- data.frame(w = sample(1:10, n, replace = TRUE),
                     v = sample(4:12, n, replace = TRUE))
 
-system.time(brute_force_knapsack(stuff,25))
+system.time(brute_force_knapsack(stuff[1:16,],25))
+system.time(dynamic_knapsack(stuff[1:500,],25))
+system.time(greedy_knapsack(stuff,25))
 system.time(brute_force_knapsack(stuff,25,parallel = TRUE))
